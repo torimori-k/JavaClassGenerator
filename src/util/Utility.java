8d9e290ece287c2036667162d1197dd5289be0dc
torimori-k <satori.k456@gmail.com>
2022-03-16 19:26:41 -0700

    add a function to check an empty list; start building AttributeDef list in ASTBuilder

--- src/util/Utility.java
+++ src/util/Utility.java
@@ -1,6 +1,9 @@
 package util;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Utility {
     public static String setOutputFileLocation(String className){
@@ -9,5 +12,17 @@ public class Utility {
 
     public static void log(String ctx) {
         System.out.println(ctx);
+    }
+
+    // reference: https://stackoverflow.com/questions/2250031/null-check-in-an-enhanced-for-loop
+    /**
+     * Use Case: to avoid null exception while using foreach loop.
+     *
+     * @param list: List type collection to be checked if null or not
+     * @param <T>: variable type for the target List
+     * @return original list if not empty, else empty list
+     */
+    public static <T> List<T> getSafeList(List<T> list) {
+        return list == null ? Collections.emptyList(): list;
     }
 }
